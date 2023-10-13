47258e521899333ad9870caf9e8d7447ff711e2d
marcus_feitosa <marcusfeitosa02@gmail.com>
2023-10-13 15:14:34 -0300

    Validando autenticação do usuário

--- src/main/java/br/com/marcusfeitosa/todo_java/filter/FilterTaskAuth.java
+++ src/main/java/br/com/marcusfeitosa/todo_java/filter/FilterTaskAuth.java
@@ -1,8 +1,11 @@
 package br.com.marcusfeitosa.todo_java.filter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.marcusfeitosa.todo_java.user.IUserRepository;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,17 +14,41 @@ import java.util.Base64;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
-
+    @Autowired
+    IUserRepository userRepository;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-       var authorization = request.getHeader("Authorization");
-       var encryptedUserPassword = authorization.substring("Basic".length()).trim();
-       byte[] decryptedUserPassword = Base64.getDecoder().decode(encryptedUserPassword);
-       var decryptedUserPasswordString = new String(decryptedUserPassword);
-       String[] userCredentials = decryptedUserPasswordString.split(":");
-       String username = userCredentials[0];
-       String password = userCredentials[1];
 
-        filterChain.doFilter(request, response);
+
+        var servelPath = request.getServletPath();
+        if(servelPath.equals("/tasks")){
+            System.out.println("Entrei no filtro");
+            var authorization = request.getHeader("Authorization");
+            var encryptedUserPassword = authorization.substring("Basic".length()).trim();
+            byte[] decryptedUserPassword = Base64.getDecoder().decode(encryptedUserPassword);
+            var decryptedUserPasswordString = new String(decryptedUserPassword);
+            String[] userCredentials = decryptedUserPasswordString.split(":");
+            String username = userCredentials[0];
+            String password = userCredentials[1];
+
+            var databaseUsername = this.userRepository.findByUsername(username);
+
+            if( databaseUsername == null){
+                response.sendError(401, "Nome de Usuário não encontrado");
+            }else{
+                System.out.println("Entrei no else do usuário");
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), databaseUsername.getPassword());
+                if(passwordVerify.verified){
+                    System.out.println("validei a senha");
+                    filterChain.doFilter(request, response);
+                }else{
+                    response.sendError(401);
+                }
+            }
+        }else{
+            System.out.println("Não segui o baile");
+            filterChain.doFilter(request, response);
+        }
     }
+
 }
--- src/main/java/br/com/marcusfeitosa/todo_java/user/IUserRepository.java
+++ src/main/java/br/com/marcusfeitosa/todo_java/user/IUserRepository.java
@@ -7,4 +7,5 @@ import java.util.UUID;
 public interface IUserRepository extends JpaRepository<UserDTO, UUID> {
     UserDTO findByUsername(String username);
 
+
 }

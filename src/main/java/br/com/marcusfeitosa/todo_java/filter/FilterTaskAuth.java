963f314d75e8de0658699ed4d9ac6f9700926c96
marcus_feitosa <marcusfeitosa02@gmail.com>
2023-10-13 13:49:33 -0300

    Decode do login de usuário

--- src/main/java/br/com/marcusfeitosa/todo_java/filter/FilterTaskAuth.java
+++ src/main/java/br/com/marcusfeitosa/todo_java/filter/FilterTaskAuth.java
@@ -1,15 +1,27 @@
 package br.com.marcusfeitosa.todo_java.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
+
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("Chegou no filtro");
-        filterChain.doFilter(servletRequest, servletResponse);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+       var authorization = request.getHeader("Authorization");
+       var encryptedUserPassword = authorization.substring("Basic".length()).trim();
+       byte[] decryptedUserPassword = Base64.getDecoder().decode(encryptedUserPassword);
+       var decryptedUserPasswordString = new String(decryptedUserPassword);
+       String[] userCredentials = decryptedUserPasswordString.split(":");
+       String username = userCredentials[0];
+       String password = userCredentials[1];
+
+        filterChain.doFilter(request, response);
     }
 }
